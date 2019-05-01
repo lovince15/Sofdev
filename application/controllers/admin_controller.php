4ecabddb5982b604ff8b4261655c38642f2614b3
darwinopulencia <dgopulencia@gmail.com>
2019-05-01 03:43:41 -0700

    Update admin_controller.php

--- application/controllers/admin_controller.php
+++ application/controllers/admin_controller.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script acces allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Admin_controller extends CI_Controller {
 
