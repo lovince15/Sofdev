c314b026ba819083d05295217072b3a039ef2355
Earl Jerome Rocero <earljeromerocero@gmail.com>
2019-05-08 14:43:16 +0800

    Update - Database - Pair Programmed

--- sports.sql
+++ sports.sql
@@ -1,11 +1,11 @@
- -- phpMyAdmin SQL Dump
--- version 4.8.3
+-- phpMyAdmin SQL Dump
+-- version 4.8.5
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 06, 2018 at 12:14 PM
--- Server version: 10.1.35-MariaDB
--- PHP Version: 7.2.9
+-- Generation Time: May 08, 2019 at 08:36 AM
+-- Server version: 10.1.39-MariaDB
+-- PHP Version: 7.3.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -62,13 +62,46 @@ CREATE TABLE `category` (
 --
 
 INSERT INTO `category` (`id`, `category_name`, `brands_id`, `date_created`) VALUES
-(15, 'Yoga', 0, '04/25/19'),
-(16, 'Boxing', 0, '04/25/19'),
+(15, '123', 0, '05/08/19'),
 (17, 'Weightlifting', 0, '04/25/19');
 
 -- --------------------------------------------------------
 
+--
+-- Table structure for table `equipment`
+--
 
+CREATE TABLE `equipment` (
+  `id` int(20) NOT NULL,
+  `equipment_name` varchar(255) NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `equipment`
+--
+
+INSERT INTO `equipment` (`id`, `equipment_name`, `date_created`) VALUES
+(6, 'EAR', '2005-08-19');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `facility`
+--
+
+CREATE TABLE `facility` (
+  `id` int(20) NOT NULL,
+  `facility_name` varchar(255) NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `facility`
+--
+
+INSERT INTO `facility` (`id`, `facility_name`, `date_created`) VALUES
+(6, 'RQEG', '2005-08-19');
 
 -- --------------------------------------------------------
 
@@ -87,10 +120,41 @@ CREATE TABLE `locations` (
 --
 
 INSERT INTO `locations` (`id`, `location_name`, `date_created`) VALUES
-(37, 'Evangelista, Makati', '04/25/19'),
-(38, 'Cash and Carry Makati', '04/25/19');
+(40, '125', '05/08/19');
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `member`
+--
+
+CREATE TABLE `member` (
+  `id` int(20) NOT NULL,
+  `member_name` varchar(255) NOT NULL,
+  `member_gender` varchar(255) NOT NULL,
+  `member_email` varchar(255) NOT NULL,
+  `member_contact` varchar(255) NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `trainer`
+--
+
+CREATE TABLE `trainer` (
+  `id` int(20) NOT NULL,
+  `trainer_name` varchar(255) NOT NULL,
+  `date_created` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `trainer`
+--
+
+INSERT INTO `trainer` (`id`, `trainer_name`, `date_created`) VALUES
+(3, '123', '2005-08-19');
 
 --
 -- Indexes for dumped tables
@@ -108,6 +172,17 @@ ALTER TABLE `admin`
 ALTER TABLE `category`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `equipment`
+--
+ALTER TABLE `equipment`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `facility`
+--
+ALTER TABLE `facility`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `locations`
@@ -115,6 +190,17 @@ ALTER TABLE `category`
 ALTER TABLE `locations`
   ADD PRIMARY KEY (`id`);
 
+--
+-- Indexes for table `member`
+--
+ALTER TABLE `member`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `trainer`
+--
+ALTER TABLE `trainer`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -130,22 +216,37 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `category`
 --
 ALTER TABLE `category`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+
+--
+-- AUTO_INCREMENT for table `equipment`
+--
+ALTER TABLE `equipment`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `facility`
 --
+ALTER TABLE `facility`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
 
 --
--- AUTO_INCREMENT for table `locations_facility`
+-- AUTO_INCREMENT for table `member`
 --
+ALTER TABLE `member`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
+--
+-- AUTO_INCREMENT for table `trainer`
+--
+ALTER TABLE `trainer`
+  MODIFY `id` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
